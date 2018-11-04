f21811b7ff8c5b01ed6c9f5ff65a22ba221b9e94
Morten Piibeleht <morten.piibeleht@gmail.com>
2018-11-04 19:24:03 +1300

    WIP: unit testing

--- src/matrixelements/g2k_lib92.f90
+++ src/matrixelements/g2k_lib92.f90
@@ -155,6 +155,93 @@ contains
         call ALCBUF(1)
     end
 
+    subroutine lib92_init_csls(cfile)
+        use g2k_csls, only: count_blocks
+        !use g2k_lib92_common
+        use memory_man
+        use alcbuf_I
+        use setcsll_I
+        use lodcsh_I
+        use lodcsh2_I
+        use setiso_I
+        use setqic_I
+        use radgrd_I
+        use nucpot_I
+        use setrwfa_I
+        use factt_I
+        use setmc_I
+        use setcon_I
+
+        character(*), intent(in) ::  cfile
+        character(:), allocatable :: cfile_local
+        integer :: ncore, nblocks
+        integer :: status
+
+        character(8), allocatable :: idblk(:)
+
+        ! The following is based on the SETCSL routine from RCI.
+        call count_blocks(cfile, nblocks, status)
+        allocate(idblk(nblocks))
+        !CALL ALLOC(pncfblk, nblocks, 4)
+        CALL ALLOC(ncfblk, nblocks, 'ncfblk', 'lib92_init_cw')
+        ! TODO: SETCSLL is pretty trivial. It could be re-written to be more sane.
+        ! NOTE: SETCSL always allocating N+1 elements.. I decided to go with N,
+        ! but unsure if it has some reason maybe?
+        ! NOTE: In SETCSL the second to last argument (NCF) was called ncftot in
+        ! the ORB2 common block.
+        cfile_local = cfile
+        CALL SETCSLL(21, cfile_local, nblocks, NBLOCK, NCFBLK, NCF, idblk)
+        ! As we already have `nblock` elements in `ncfblk`, there is no need
+        ! to re-allocate, as was done in SETCSL.
+        REWIND (21)
+        READ (21,*) ! WTH? But LODCSH fails without this.
+        CALL LODCSH (21, ncore)
+        ! Ok, LODCSH only loads the _header_ of the CSL file. Among probably other
+        ! stuff, it leaves the JCUPA variable undefined, causing segfaults.
+        !
+        ! Ok.. LODCSH2 seems like it might load the CSFs in. It requires
+        !   - ORB2/PNTRIQ
+        !   - STAT/PNTJQS
+        !   - STAT/PNJCUP
+        ! to be allocated though. The allocation was taken from matrix.f.
+        !
+        ! NOTE: to get it working, before I figure LODCSH2, I also tried
+        !   call LODCSL(ncore)
+        !   call LODCSL(21, ncore)
+        !   call LODCSL2(21, ncore, 1)
+        !
+        ! These are from the GRASP2K implementation..
+        ! CALL ALLOC(PNTRIQ, NNNWP*NCF, 4)
+        ! CALL ALLOC(PNTJQS, NNNWP*NCF*3, 4)
+        ! CALL ALLOC(PNJCUP, NNNWP*NCF, 4)
+        !
+        ! There are from matrix.f
+        CALL ALLOC(IQA, NNNW, ncf, 'IQA', 'MATRIX')
+        CALL ALLOC(JQSA, NNNW, 3, ncf, 'JQSA', 'MATRIX')
+        CALL ALLOC(JCUPA, NNNW, ncf, 'JCUPA', 'MATRIX')
+        ! CALL ALLOC (SLF_EN,ncf,'SLF_EN', 'MATRIX')
+        ! CALL ALLOC (UCF, nw,'UCF', 'MATRIX')
+        CALL LODCSH2(21, ncore, -119)
+    end subroutine lib92_init_csls
+
+    subroutine lib92_init_rkco_gg
+        use alcbuf_I
+        use factt_I
+
+        ! Set up the table of logarithms. From rci3mpi.f
+        !
+        ! This sets up the FACTS common block, which is used by the CORD routine
+        ! that gets passed to RKCO_GG.
+        call FACTT
+
+        ! RKCO_GG uses the BUFFER common block in it's dependencies, but only
+        ! re-allocates via ALCBUF. ALCBUF is actually decently documented in a
+        ! sense -- it makes it relatively clear that you need to call ALCBUF(1)
+        ! at some point. In rci_mpi it is done in SETHAM, before the main loop.
+        ! So we'll do the allocation here.
+        call ALCBUF(1)
+    end
+
     !> Initializes the common blocks necessary for calling rcicommon routines.
     subroutine rcicommon_init
         use grasp_kinds, only: real64
--- test/matrixelements_hydrogenic.f90
+++ test/matrixelements_hydrogenic.f90
@@ -1,30 +1,58 @@
 program matrixelements_hydrogenic
     use grasp_kinds, only: real64, dp
+    use g2k_lib92, only: lib92_init_csls, lib92_init_rkco_gg, rcicommon_init
     use grasptest_lib9290_setup
     use grasptest_lib9290_hydrogenic
-    use orbout_I
     implicit none
 
     type(orbital_definition), parameter, dimension(*) :: orbitals = (/ &
         orbital_definition(1, -1), & ! 1s
         orbital_definition(2, -1), & ! 2s
         orbital_definition(2,  1), & ! 2p-
-        orbital_definition(2, -2), & ! 2p
-        orbital_definition(3,  2), & ! 3d-
-        orbital_definition(3, -3), & ! 3d
-        orbital_definition(4,  3), & ! 4f-
-        orbital_definition(5, -4)  & ! 5f
+        orbital_definition(2, -2)  & ! 2p
     /)
 
     logical :: tests_passed = .true.
     integer :: n, k, l, tmpk, tmpl
     real(real64) :: vp_value
+    character(:), allocatable :: testdata
+
+    if(.not.getenv_allocating("RCIQED_TESTDATA", testdata)) then
+        error stop
+    endif
+    print '("RCI_TESTDATA=",a)', testdata
 
     call setup(74.0_dp)
     call allocate_hydrogenic_orbitals(orbitals)
-    call orbout("hydrogenic.w")
+
+    call lib92_init_csls(testdata//"/oxygen.c")
+    call lib92_init_rkco_gg
+    call rcicommon_init
+
+    call calculate_hij(1, 1)
+    call calculate_hij(1, 2)
+    call calculate_hij(2, 2)
+    call calculate_hij(3, 3)
+    call calculate_hij(4, 4)
+    call calculate_hij(4, 5)
+    call calculate_hij(5, 5)
 
 contains
+
+    subroutine calculate_hij(ic, ir)
+        use grasp_cimatrixelements
+
+        integer, intent(in) :: ir, ic
+        real(real64) :: hij_dp, hij_coulomb, hij_breit
+
+        hij_dp = dirac_potential(ic, ir)
+        hij_coulomb = coulomb(ic, ir)
+        hij_breit = breit(ic, ir)
+
+        print '("H(",i0,",",i0"):")', ic, ir
+        print *, hij_dp, hij_coulomb, hij_breit
+        print '(d30.16)', hij_dp + hij_coulomb + hij_breit
+    end subroutine calculate_hij
 
     function reldiff(a, b)
         use grasp_kinds, only: real64
@@ -47,5 +75,46 @@ contains
             tests_passed = .false.
         endif
     end subroutine check_tolerance
+
+    !> Fetches an environment variable.
+    !!
+    !! If it was able to fetch the variable value, returns `.true.` and sets
+    !! `value` to the value ([re]allocating if necessary). Returns `.false.` if
+    !! the variable is not defined
+    !!
+    !! Under the hood it calls `get_environment_variable`, but it properly
+    !! allocates or re-allocates the `value` to match the actual length of the
+    !! environment variable.
+    !!
+    !! TODO: For this to be a proper library function, it should be implemented
+    !! as an interface with additional methods to handle pointers and fixed-length
+    !! strings.
+    function getenv_allocating(variable_name, value)
+        logical :: getenv_allocating
+        character(*), intent(in) :: variable_name
+        character(:), allocatable, intent(inout) :: value
+        integer :: length, status
+        character(1) :: test
+
+        ! First, make an inquiry call to get_environment_variable to determine
+        ! whether the variable exists and, if so, its length.
+        call get_environment_variable(variable_name, test, length, status)
+        if(status > 0) then
+            ! status of 1 or 2 from get_environment_variable means that the the
+            ! variable is undefined, or that the system does not support
+            ! environment variables at all, respectively. Both situations make
+            ! getenv fail.
+            getenv_allocating = .false.
+            return
+        endif
+        ! Will allocate or re-allocate value, unless it already has the correct length.
+        if(allocated(value) .and. len(value) /= length) deallocate(value)
+        if(.not.allocated(value)) allocate(character(length) :: value)
+        ! Can't pass a length 0 string to get_environment_variable.
+        if(length > 0) then
+            call get_environment_variable(variable_name, value, length, status)
+        endif
+        getenv_allocating = .true.
+    end
 
 end program matrixelements_hydrogenic
