c2026213ecf3fe228c2ed1dde950e97e89b01728
Morten Piibeleht <morten.piibeleht@gmail.com>
2018-11-04 17:53:51 +1300

    Move exportvp to tools/

--- tools/exportvp.f90
+++ tools/exportvp.f90
@@ -1,5 +1,4 @@
 program exportvp
-    use grasp_rci_utils, only: write_zdist_csv
     implicit none
     call setup
     call setup_vacuum_polarization
@@ -74,5 +73,20 @@ contains
         FRSTVP = .TRUE.
         NVPI = 0
     end subroutine setup_vacuum_polarization
+
+    subroutine write_zdist_csv
+        use ncdist_C, only: ZDIST
+        use grid_C, only: N, R, RP
+        implicit none
+
+        integer :: i, fh
+
+        open(newunit=fh, file="zdist.csv", action='write')
+        write(fh, '(a5,3(",",a25))') "idx", "r", "rp", "zdist"
+        do i = 1, N
+            write(fh, '(i5,3(",",es25.16))') i, R(i), RP(i), ZDIST(i)
+        enddo
+        close(fh)
+    end subroutine write_zdist_csv
 
 end program exportvp
