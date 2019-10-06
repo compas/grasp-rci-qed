8313803ef2825202a78261f56fb5485c4af61cb4
Morten Piibeleht <morten.piibeleht@gmail.com>
2019-10-06 17:35:31 +1300

    Clean test.jl

--- test/julia/test.jl
+++ test/julia/test.jl
@@ -9,8 +9,7 @@
 #
 # in the developer tools of Atom, and then restarting the Julia process.
 #
-using Test
-@test ENV["GFORTRAN_CONVERT_UNIT"] === "big_endian"
+@assert ENV["GFORTRAN_CONVERT_UNIT"] === "big_endian"
 using RCIQED
 RCIQED.__reload__()
 
@@ -29,9 +28,20 @@ RCIQED.globals_orbitals()
 RCIQED.globals(:orb_C, :NCF) # Number of CSFs
 RCIQED.globals(:prnt_C, :NVEC) # Number of ASFs
 
-# Compute the single-particle matrix elements of the QED SE operators
-ops = [RCIQED.qedse(i) for i=1:4]
+# Operators
+ops = [
+    RCIQED.diracpot, RCIQED.coulomb,
+    RCIQED.breit,
+    RCIQED.nms, RCIQED.sms,
+    RCIQED.qedvp,
+    (RCIQED.qedse(i) for i=1:4)...
+]
+RCIQED.asfvalues(ops)
 
+
+# ==============================================================================
+
+RCIQED.matrixelement(ops[3], 1, 1)
 
 dpop = RCIQED.diracpot()
 RCIQED.asfvalue(dpop, 1)
@@ -46,53 +56,6 @@ op = RCIQED.qedse(2)
 size(op)
 RCIQED.materialize(op)
 
-ops = [
-    RCIQED.diracpot, RCIQED.coulomb,
-    RCIQED.breit,
-    RCIQED.nms, RCIQED.sms,
-    RCIQED.qedvp,
-    (RCIQED.qedse(i) for i=1:4)...
-]
-RCIQED.asfvalues(ops)
-
 RCIQED.onescalar(1,1)
 
-RCIQED.matrixelement(ops[2], 1, 1)
-
 RCIQED.asfcoefficients()
-
-# @testset "libgrasp-rci" begin
-#     @test_throws ErrorException grasp_load_isodata("isodata.Z60")
-#     @test grasp_initialize_constants() === nothing
-#     @test grasp_load_isodata("test/isodata") === nothing
-#     @test grasp_load_csl("test/test.c") === nothing
-#     # Note: requires isodata and CSL
-#     @test grasp_load_orbitals("test/test.w") === nothing
-#     @test grasp_init_rkco_gg() === nothing
-#     @test grasp_init_dcb() === nothing
-#     @test grasp_load_mixing("test/test.cm") === nothing
-# end
-
-#@show grasp_libgraspci_j2max()
-#@show grasp_libgraspci_j2max()
-
-# @show grasp_orbital_grid(1)
-# @show grasp_orbital_grid(2)
-# @show grasp_orbital_grid(3)
-# @show grasp_orbital_grid(4)
-#
-# println("NCF = $(grasp_ncsfs())")
-#
-# @show grasp_ci_coulomb(1, 1)
-# @show grasp_ci_diracnuclear(1, 1)
-#
-# println("End of script.")
-#
-# grasp_orbitals()
-
-
-
-# @show grasp_orbitals()
-# m = grasp_ci_qedse_matrix(2)
-
-RCIQED._reopen_libgrasp()
